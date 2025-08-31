763e4c9350e88243af87811d4cad713f069f1212
Arafat Sarker <mmarafatu@gmail.com>
2025-09-01 05:29:58 +0600

    20-10 Enhancing useReducer with Business Logic

--- redux-basic/src/components/Form.jsx
+++ redux-basic/src/components/Form.jsx
@@ -1,7 +1,6 @@
-import React, { useReducer, } from 'react'
+import React, { useReducer } from "react";
 
 export default function Form() {
-
   // State to hold user information
   // const [userInfo, setUserInfo] = useState({
   //   name: '',
@@ -9,89 +8,115 @@ export default function Form() {
   //   number: ''
   // });
 
-
   // Update user information based on input changes
   // const userInfoUpdate = (e) => {
   //   setUserInfo({ ...userInfo, [e.target.name]: e.target.value });
   // }
 
-
   // Using useReducer for state management
   const inisialState = {
-    name: '',
-    email: '',
-    number: ''
-  } 
+    name: "",
+    email: "",
+    number: "",
+  };
 
   // Update user information based on input changes
-  const reducer =(state, action)=>{
-      switch (action.type) {
-        case 'update':
-          return {
-            ...state,
-            [action.payload.field]: action.payload.value
-          };
+  const reducer = (state, action) => {
+    switch (action.type) {
+      // Update specific field in the state
+      case "update":
+        return {
+          ...state,
+          [action.payload.field]: action.payload.value,
+        };
 
-          default: state;
-      }
-  }
+        // Clear specific field in the state
+      case "clear":
+        return {
+          ...state,
+          [action.payload.field]: "",
+        };
+
+        // Reset state to initial values
+        case "reset":
+          return inisialState;
+
+      default:
+        state;
+    }
+  };
 
   // Function to handle input changes
   const [userInfo, dispatch] = useReducer(reducer, inisialState);
+  console.log(userInfo);
 
-  const handelOnChange = (e) =>{
+
+  // Handle clearing specific field
+    const handelClear = (field) => {
     dispatch({
-      type: 'update',
+      type: "clear",
       payload: {
-            field: e.target.name,
-            value: e.target.value
-          }
-    })
+        field: field,
+      },
+    });
+  };
 
-    }
+  // Handle input change
+  const handelOnChange = (e) => {
+    dispatch({
+      type: "update",
+      payload: {
+        field: e.target.name,
+        value: e.target.value,
+      },
+    });
+  };
 
   // Handle form submit
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(userInfo);
-  }
+  };
 
   return (
     <div>
-      // Simple Form
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name</label>
-          <input 
-            type="text" 
+          <input
+            type="text"
             name="name"
             value={userInfo.name}
             onChange={handelOnChange}
           />
+          <button onClick={()=> handelClear("name")}>Clear</button>
         </div>
         <br />
         <div>
           <label htmlFor="email">Email</label>
-          <input 
-            type="text" 
+          <input
+            type="text"
             name="email"
             value={userInfo.email}
             onChange={handelOnChange}
           />
+             <button onClick={()=> handelClear("email")}>Clear</button>
         </div>
         <br />
         <div>
           <label htmlFor="number">Number</label>
-          <input 
-            type="text" 
+          <input
+            type="text"
             name="number"
             value={userInfo.number}
             onChange={handelOnChange}
           />
+             <button onClick={()=> handelClear("number")}>Clear</button>
         </div>
         <br />
         <button type="submit">Submit</button>
+        <button onClick={()=> dispatch({type: "reset"})}>Reset</button>
       </form>
     </div>
-  )
+  );
 }
