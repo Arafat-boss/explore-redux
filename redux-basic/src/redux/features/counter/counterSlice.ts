47959d1e11827b22ccd7fc0b30469d2a356f4c21
Arafat Sarker <mmarafatu@gmail.com>
2025-09-02 06:06:13 +0600

    i am create a contureSlice funtion with type and initialSate with value

--- redux-basic/src/redux/features/counter/counterSlice.ts
+++ redux-basic/src/redux/features/counter/counterSlice.ts
@@ -1,7 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
+const initialState = {
+    value: 0
+}
 const counterSlice = createSlice({
     name: "copunter",
-    initialState
+    initialState,
 })
