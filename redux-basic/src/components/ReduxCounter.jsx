31627637477b31b83050ffa812782e022d75dea8
Arafat Sarker <mmarafatu@gmail.com>
2025-09-04 03:04:57 +0600

    now i am successfully added handelIncrement

--- redux-basic/src/components/ReduxCounter.jsx
+++ redux-basic/src/components/ReduxCounter.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
+import { increment } from '../redux/features/counter/counterSlice';
 
 export default function ReduxCounter() {
   
@@ -8,7 +9,7 @@ export default function ReduxCounter() {
 
   // Action creator for incrementing
   const handelIncrement =()=>{
-    dispstch(incre)
+    dispstch(increment())
   }
 
   return (
